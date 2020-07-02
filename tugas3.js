a56bc129621b805cb1a476c3efa76d53523fa405
hugoiman <hugogiman17@gmail.com>
2020-07-02 10:17:48 +0700

    Tugas 3 Javascript

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+var item = "Sebelum di ubah";
+
+var change = ubah(item);
+
+console.log(item);
+console.log(change);
+
+function ubah(nilai) {
+  nilai = "Variabel ini sudah diubah";
+  return nilai;
+}
